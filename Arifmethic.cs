43429235a3369f7ed4cddb4407c4ea48f92cc844
Clayfish <maxmetel@gmail.com>
2015-07-06 15:51:49 +0600

    Added Compute function

---
+++ Arifmethic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Arifmethic
+{
+	public Arifmethic()
+	{
+	}
+
+    public String Compute(String A, String B, char op)
+    {
+         double firstArgument, secondArgument;
+        if (double.TryParse(A, out firstArgument) &&
+            double.TryParse(B, out secondArgument))
+        {
+            switch (op)
+            {
+                case '+': return Convert.ToString(firstArgument + secondArgument);
+                case '-': return Convert.ToString(firstArgument - secondArgument);
+                case '*': return Convert.ToString(firstArgument * secondArgument);
+                case '/': return Convert.ToString(firstArgument / secondArgument);
+
+            }
+        }
+        else
+        {
+            return "Error";
+        }
+    }
+
+}
