763953dbf5e0473969227b1b10776046a7b1ae2b
Mas'ya` <maxmetel@gmail.com>
2015-07-09 12:57:01 +0600

    Factory tests

--- calculator420.Tests/OneArgument/OneArgumentTests.cs
+++ calculator420.Tests/OneArgument/OneArgumentTests.cs
@@ -1,7 +1,31 @@
-﻿namespace calculator.Tests
+﻿using System;
+using Calculator.OneArgument;
+using NUnit.Framework;
+
+namespace calculator.Tests
 {
+    [TestFixture]
     public class OneArgumentTests
     {
-         
+        [TestCase(typeof(Abs), "Abs")]
+        [TestCase(typeof(Sin), "Sin")]
+        [TestCase(typeof(Cos), "Cos")]
+        [TestCase(typeof(Tan), "Tan")]
+        [TestCase(typeof(Arcsin), "Arcsin")]
+        [TestCase(typeof(Arccos), "Arccos")]
+        [TestCase(typeof(Arctan), "Arctan")]
+        [TestCase(typeof(Sqrt), "Sqrt")]
+        public void FactoryTest(Type type, string name)
+        {
+            Type resultType = OneArgmumentFactory.CreateCalculator(name).GetType();
+            Assert.AreEqual(type, resultType);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void UndefTest()
+        {
+            Type resultType = OneArgmumentFactory.CreateCalculator("Ass").GetType();
+        }
     }
 }
--- calculator420.Tests/TwoArgument/TwoArgumentTests.cs
+++ calculator420.Tests/TwoArgument/TwoArgumentTests.cs
@@ -1,7 +1,26 @@
-﻿namespace calculator.Tests
+﻿using System;
+using Calculator.TwoArgument;
+using NUnit.Framework;
+
+namespace calculator.Tests
 {
+    [TestFixture]
     public class TwoArgumentTests
     {
-         
+        [TestCase(typeof(Add), "Add")]
+        [TestCase(typeof(Div), "Div")]
+        [TestCase(typeof(Sub), "Sub")]
+        [TestCase(typeof(Mult), "Mult")]
+        [TestCase(typeof(Pow), "Pow")]
+        [TestCase(typeof(Root), "Root")]
+        public void FactoryTest(Type type, string name)
+        {
+            Type resultType = TwoArgmumentFactory.CreateCalculator(name).GetType();
+            Assert.AreEqual(type, resultType);
+        }
+        public void UndefTest()
+        {
+            Type resultType = TwoArgmumentFactory.CreateCalculator("Ass").GetType();
+        }
     }
 }
